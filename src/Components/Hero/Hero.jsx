14741dc56712d0fda603a34705d2b67565c5a97f
Besir Kurtishi <kurtishi45@gmail.com>
2023-02-27 23:34:41 +0100

    added react typical

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,6 +4,7 @@ import HeroImg from '../../assets/heroPicture.webp';
 import Suuber from '../../assets/partners/suuber_logo.png';
 import Wpengine from '../../assets/partners/wpengine.png';
 import Demogency from '../../assets/partners/demogency.webp';
+import Typical from 'react-typical';
 
 const Hero = () => {
 
@@ -21,14 +22,20 @@ const Hero = () => {
     };
     const item = {
         hidden: { opacity: 0 },
-        show: { opacity: 1, transition: { duration: 0.5, } },
+        show: { opacity: 1, transition: { duration: 0.3, } },
     };
 
     return (
         <>
             <motion.div variants={Container} initial="hidden" animate="show" className='phormalabs__hero section-padding'>
                 <div className='phormalabs__hero-content '>
-                    <motion.h1 variants={item}>Digitalize your Business</motion.h1>
+                    <motion.h1 variants={item}>Digitalize your <br></br>
+                        <Typical
+                            steps={[500, "Business", 1000, "Brand", 1000]}
+                            loop={Infinity}
+                            wrapper="span"
+                        />
+                    </motion.h1>
                     <motion.p variants={item}>Ready to take your brand on a journey of digital success? Let's create a unique digital strategy that sets you apart from the competition and delivers measurable results.</motion.p>
                     <motion.a variants={item} href="#contact" className='button'>Get Started</motion.a>
                 </div>
