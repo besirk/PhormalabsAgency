f2f4d5c045f133e1ce57b7e360053be6afddd504
Besir Kurtishi <kurtishi45@gmail.com>
2023-03-04 09:15:05 +0100

    updated footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,10 @@
 import './footer.css';
-import Logo from "../../assets/phormalabsLogoSVG.svg";
+import Logo from "../../assets/viteWhite.svg";
 import { motion } from 'framer-motion';
+import fbb from '../../assets/social/Vector.svg'
+import inst from '../../assets/social/insta.svg'
+import lin from '../../assets/social/linkedin.svg'
+import loc from '../../assets/social/loc.svg'
 
 
 const Footer = () => {
@@ -19,11 +23,23 @@ const Footer = () => {
     return (
         <>
             <div className='phormalabs__footer' id='footer'>
-                <div className='logoFooter'>
-                    <img src={Logo} alt="Phormalabs Logo" />
-                    <span>Phormalabs</span>
+                <div className="footer__content">
+                    <div className='logoFooter'>
+                        <img src={Logo} alt="Phormalabs Logo" />
+                        <span>Phormalabs</span>
+                    </div>
+                    <p>Turn your crazy ideas into the best business model</p>
+                    <div className="social_media">
+                        <ul>
+                            <li><a href="https://www.facebook.com/phormalabs"><img src={fbb} alt="Facebook icon" /></a></li>
+                            <li><a href="https://www.instagram.com/phormalabs"><img src={inst} alt="Instagram icon" /></a></li>
+                            <li><a href="https://www.linkedin.com/company/phormalabs/"><img src={lin} alt="Linkedin icon" /></a></li>
+                            <li><a href="https://www.facebook.com/phormalabs"><img src={loc} alt="Maps icon" /></a></li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="phormalabs__footer-links">
+                    <h3>Links</h3>
                     <ul>
                         {links.map((link, index) => (
                             <motion.li key={index} variants={item}>
@@ -33,6 +49,18 @@ const Footer = () => {
                             </motion.li>
                         ))}
                     </ul>
+
+                </div>
+                <div className="phormalabs__footer-links aaa">
+                    <h3>Services</h3>
+                    <ul>
+                        <li>Web Development</li>
+                        <li>Logo Design</li>
+                        <li>Social Media Management</li>
+                        <li>Business Branding</li>
+                        <li>Social Media Marketing</li>
+                    </ul>
+
                 </div>
             </div>
             <div className='phormalabs__copyright'>
