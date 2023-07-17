b2b9f067e386a5929aea4c60364af9ea9698f42a
besirk <kurtishi45@gmail.com>
2023-07-18 01:08:29 +0200

    added ai chatbot to phormalabs agency

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Navbar, Hero, AboutUs, Statistics, Projects, Cta, Footer } from './Components/'
+import {Helmet} from "react-helmet";
 
 function App() {
 
@@ -11,7 +12,10 @@ function App() {
       <Projects />
       <Cta />
       <Footer />
-
+      <Helmet>
+      <script src="https://cdn.botpress.cloud/webchat/v0/inject.js"></script>
+<script src="https://mediafiles.botpress.cloud/1a1fc98a-68df-4269-8e92-e66ef9afaedc/webchat/config.js" defer></script>
+      </Helmet>
     </div>
   )
 }
