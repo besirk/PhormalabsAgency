e7a2926bbba1a6663ec4ca6b433d912439b8e071
besirk <kurtishi45@gmail.com>
2023-07-21 21:45:04 +0200

    added custom css

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import {
   Navbar,
   Hero,
@@ -7,9 +8,39 @@ import {
   Cta,
   Footer,
 } from "./Components/";
-import { Helmet } from "react-helmet";
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://cdn.botpress.cloud/webchat/v0/inject.js";
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.botpressWebChat.init({
+        composerPlaceholder: "Chat with Lexus",
+        botConversationDescription: "Hi! I'm Lexus, your AI Assistant from Phormalabs",
+        botId: "1a1fc98a-68df-4269-8e92-e66ef9afaedc",
+        hostUrl: "https://cdn.botpress.cloud/webchat/v0",
+        messagingUrl: "https://messaging.botpress.cloud",
+        clientId: "1a1fc98a-68df-4269-8e92-e66ef9afaedc",
+        botName: "Lexus Phormalabs",
+        phoneNumber: "+389 70 362 182",
+        emailAddress: "contact@phormalabs.com",
+        website: "https://phormalabs.com",
+        stylesheet: "https://www.phormalabs.com/assets/index-d7a68970.css",
+        useSessionStorage: true,
+      });
+    };
+
+
+
+    return () => {
+      // Clean up the script when the component is unmounted
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
@@ -19,13 +50,6 @@ function App() {
       <Projects />
       <Cta />
       <Footer />
-      <Helmet>
-        <script src="https://cdn.botpress.cloud/webchat/v0/inject.js"></script>
-        <script
-          src="https://mediafiles.botpress.cloud/1a1fc98a-68df-4269-8e92-e66ef9afaedc/webchat/config.js"
-          defer
-        ></script>
-      </Helmet>
     </div>
   );
 }
